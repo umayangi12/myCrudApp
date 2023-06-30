b351bf8ef4f69edf7c2e46c0c7937d19bb5f16d7
Umayangi Ekanayake <umayangi1999@gmail.com>
2023-06-30 20:22:28 +0530

    property creation

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 
 //invoking express
 const app = express();
 
-const port = 8000;
+const PORT = 8000;
+const DB_URL =
+  "mongodb+srv://uma:uma123@cluster0.jmecq7c.mongodb.net/myMernApp?retryWrites=true&w=majority";
 
-app.listen(port, () => {
-    console.log(`App is running on ${port}`);
+mongoose
+  .connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+
+  .then(() => {
+    console.log("DB connected...");
+  })
+  .catch((error) => console.log("DB connection error", error));
+
+app.listen(PORT, () => {
+  console.log(`App is running on ${PORT}`);
 });
