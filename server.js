3a1e09e8aa5e7f90bd6aa35a932fc13b09ad4380
Umayangi Ekanayake <umayangi1999@gmail.com>
2023-07-01 13:17:55 +0530

    created initial backend

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const podyParser = require('pody-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+const app = express();
+
